b8907df37272921379e9aa8a1406616eccff328e
Valinda Chan <valinda.chan@gmail.com>
2017-07-07 18:15:27 -0400

    Solved the first part of displaying the average price

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-// Since the `data.js` file is loaded in your `index.html` before this one, 
+// Since the `data.js` file is loaded in your `index.html` before this one,
 // you have access to the `data` variable that contains the Etsy Store data.
 // Open your `index.html` page in your browser and you can verify that the following
 // is properly working. The `data` variable is an array with 25 items in it
 console.log(data);
+// console.log(question1());
 
 
 
 // 1: Show me how to calculate the average price of all items.
 function question1 () {
   // Answer:
+  let sum = 0;
+
+  for (var i = 0; i < data.length; i++) {
+    sum = sum + data[i]['price'];
+    // console.log(data[i]['price']);
+  }
+
+  let avgPrice = sum / 25; // Update this later
+
+  // avgPrice = avgPrice.toFixed(2);
+
+  console.log('The average price is $' + avgPrice.toFixed(2));
+  return avgPrice;
+
 }
 
 
@@ -32,7 +47,7 @@ function question4 () {
 }
 
 
-// 5: Which items are made of eight or more materials? 
+// 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
 function question5 () {
   // Answer:
