47a665cfedf50e24e1c3e2d549a5eb01b9546c50
Valinda Chan <valinda.chan@gmail.com>
2017-07-07 23:02:19 -0400

    Display information about the items made of eight or more materials

--- main.js
+++ main.js
@@ -104,6 +104,25 @@ function question4 () {
 //    Display the name, number of items and the items it is made of.
 function question5 () {
   // Answer:
+  let itemsWithOverEightMaterials = [];
+
+  // Go through each item
+  for (var i = 0; i < data.length; i++) {
+    // If the item is made of eight or more materials, first display the name
+    // and number of items
+    if(data[i]['materials'].length >= 8) {
+      itemsWithOverEightMaterials.push(data[i]['title'], data[i]['materials'].length, data[i]['materials']);
+
+      console.log(data[i]['title'] + ' has ' + data[i]['materials'].length + ' materials: ');
+
+      // Then display a list of the materials it is made of
+      for (var j = 0; j < data[i]['materials'][j].length; j++) {
+        console.log('- ' + data[i]['materials'][j]);
+      }
+
+    }
+  }
+
 }
 
 
